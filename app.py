faaea6017e6e365503c365ab2a8b4b2a2a23ea4c
Reimel-Maestre <reimeljhosuemaestre@gmail.com>
2026-04-18 15:20:41 -0400

    Proyecto FFX: inicial import

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# --- RUTAS DE LA APLICACIÓN ---
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/spira')
+def spira():
+    return render_template('spira.html')
+
+@app.route('/legado')
+def legado():
+    return render_template('legado.html')
+
+@app.route('/biblioteca')
+def biblioteca():
+    return render_template('biblioteca.html')
+
+# --- INICIO DEL SERVIDOR ---
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
